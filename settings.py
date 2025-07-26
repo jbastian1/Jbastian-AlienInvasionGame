556183959e86b25251f379fc757ffca411a8880e
jbastian1 <jbastian1@student.cscc.edu>
2025-07-26 13:45:04 -0400

    P2 - Created the alien, displayed on screen, no movement

--- settings.py
+++ settings.py
@@ -20,5 +20,10 @@ class Settings:
         self.bullet_h = 25                                                                          # 80 -> 25
         self.bullet_amount = 5
 
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_speed = 5
+
 
 
