d246b6063517134696a371f9925deaef3ec199bd
jbastian1 <jbastian1@student.cscc.edu>
2025-07-27 08:20:49 -0400

    p3 - set up button to start game and make mouse disappear

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from arsenal import Arsenal
 # from alien import Alien
 from alien_fleet import AlienFleet
 from time import sleep
+from button import Button
 
 class AlienInvasion:
 
@@ -39,7 +40,9 @@ class AlienInvasion:
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
         self.alien_fleet.create_fleet()
-        self.game_active = True
+
+        self.play_button = Button(self, 'Play')
+        self.game_active = False
 
     def run_game(self) -> None:
         # Game loop
@@ -88,11 +91,24 @@ class AlienInvasion:
         self.alien_fleet.fleet.empty()
         self.alien_fleet.create_fleet()
 
+    def restart_game(self):
+        # setting up dynamic settings
+        # reset Game stats
+        # update HUD scores
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
 
     def _update_screen(self) -> None:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+
         pygame.display.flip()
 
     def _check_events(self) -> None:
@@ -105,6 +121,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game()
 
     def _check_keyup_events(self, event) -> None:
         if event.key == pygame.K_UP:                                                            # K_RIGHT -> K_UP
--- button.py
+++ button.py
@@ -10,15 +10,15 @@ class Button:
     def __init__(self, game: 'AlienInvasion', msg) -> None:
         self.game = game
         self.screen = game.screen
-        self.boundaries = game.screen.game_rect()
+        self.boundaries = game.screen.get_rect()
         self.settings = game.settings
         self.font = pygame.font.Font(self.settings.font_file,
             self.settings.button_font_size)
-        self.rect = pygame.Rect(0,0,self.settings.buttom_w, self.settings.buttom_h)
+        self.rect = pygame.Rect(0,0,self.settings.button_w, self.settings.button_h)
         self.rect.center = self.boundaries.center
         self._prep_msg(msg)
 
-    def _prep_ms(self, msg) -> None:
+    def _prep_msg(self, msg) -> None:
         self.msg_image = self.font.render(msg, True, self.settings.text_color, None)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
