af1bc102a55f1e1599786d4302aa8ccfb3da8832
jbastian1 <jbastian1@student.cscc.edu>
2025-07-27 21:38:46 -0400

    p3 - created HUD to display score, max, and hi score with persistent file storage

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from arsenal import Arsenal
 from alien_fleet import AlienFleet
 from time import sleep
 from button import Button
+from hud import HUD
 
 class AlienInvasion:            # last two videos unit 13 & short question
 
@@ -15,7 +16,6 @@ class AlienInvasion:            # last two videos unit 13 & short question
         pygame.init()
         self.settings = Settings()
         self.settings.initialize_dynamic_settings()
-        self.game_stats = GameStats(self)
 
         self.screen = pygame.display.set_mode((
             self.settings.screen_w,self.settings.screen_h)
@@ -27,6 +27,8 @@ class AlienInvasion:            # last two videos unit 13 & short question
             (self.settings.screen_w, self.settings.screen_h)
             )
 
+        self.game_stats = GameStats(self)
+        self.HUD = HUD(self)
         self.running = True
         self.clock = pygame.time.Clock()
 
@@ -71,6 +73,7 @@ class AlienInvasion:            # last two videos unit 13 & short question
             self.impact_sound.play()
             self.impact_sound.fadeout(500)
             self.game_stats.update(collisions)
+            self.HUD.update_scores()
 
         if self.alien_fleet.check_destroyed_status():
            self._reset_level()
@@ -99,9 +102,8 @@ class AlienInvasion:            # last two videos unit 13 & short question
 
     def restart_game(self):
         self.settings.initialize_dynamic_settings()
-        # reset Game stats
         self.game_stats.reset_stats()
-        # update HUD scores
+        self.HUD.update_scores()
         self._reset_level()
         self.ship._center_ship()
         self.game_active = True
@@ -111,7 +113,7 @@ class AlienInvasion:            # last two videos unit 13 & short question
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
-        # draw HUD
+        self.HUD.draw()
 
         if not self.game_active:
             self.play_button.draw()
@@ -123,6 +125,7 @@ class AlienInvasion:            # last two videos unit 13 & short question
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+                self.game_stats.save_scores()
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN and self.game_active == True:
@@ -156,6 +159,7 @@ class AlienInvasion:            # last two videos unit 13 & short question
 
         elif event.key == pygame.K_q:
             self.running = False
+            self.game_stats.save_scores()
             pygame.quit()
             sys.exit()
 
